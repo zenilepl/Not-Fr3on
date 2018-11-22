b7933698cb4a822bfc258a14916eeaae4ecc06e5
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-11-22 20:39:52 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Hassan_Gamer");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("اي بي الشات")
+setInterval(function() {
+channel.send(`Not Fr3on`);
+}, 25)
+})
+ 
+ 
+client.login("NTE1MjMyNTgyNzI0NjE2MjEw.DtiHTw.LbB-5e5tSXMgc_JJQDzXPJtQgcs");

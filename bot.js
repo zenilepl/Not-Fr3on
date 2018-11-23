e9b0da6defc37bbb46b8e46ef51318a8175e1968
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-11-23 19:10:22 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Hassan_Gamer");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("513473609713319941")
+setInterval(function() {
+channel.send(`zenilep`);
+}, 25)
+})
+ 
+ 
+client.login("client.login(process.env.BOT_TOKEN);");
